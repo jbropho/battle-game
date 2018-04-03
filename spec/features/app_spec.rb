0df986440bf565f56197410b54f691e3509e2bbf
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-03 15:47:37 +0100

    pass feature test, players have deafult hitpoints at game start

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,12 +1,20 @@
-RSpec.feature "Sumbmit player names", :type => :feature do
-  scenario "User submits player names" do
+RSpec.feature "Start a new game", :type => :feature do
+  before(:each) do
     visit "/"
-
     fill_in "player_one", :with => "Dave"
     fill_in "player_two", :with => "John"
     click_button "Submit"
+  end
 
+  scenario "User submits player names" do
     expect(page).to have_text("Dave")
     expect(page).to have_text("John")
+  end
+
+  scenario "Page has player hitpoints" do
+    player_one = page.find_by_id("playerOnePoints")
+    expect(player_one).to have_text("100")
+    player_two = page.find_by_id("playerTwoPoints")
+    expect(player_two).to have_text("100")
   end
 end
