559a329364c79115817300faa3f7ca51e0418e98
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-03 18:30:02 +0100

    extract player health to instanc variable

--- app.rb
+++ app.rb
@@ -10,12 +10,16 @@ class MyApp < Sinatra::Base
   post '/names' do
     session[:player_one] = params[:player_one]
     session[:player_two] = params[:player_two]
+    session[:p1_health] = 100
+    session[:p2_health] = 100
     redirect '/play'
   end
 
   get '/play' do
     @player_one = session[:player_one]
     @player_two = session[:player_two]
+    @p1_health = session[:p1_health]
+    @p2_health = session[:p2_health]
     erb :play
   end
 end
