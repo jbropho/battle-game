76536c6154165ddaaebf0f2d243016442eb88b2a
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-03 15:18:05 +0100

    refactor POST /names to redirect to GET /play

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
+  set :sessions, true
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
-    erb(:play)
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
+    erb :play
   end
 end
