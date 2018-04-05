1d52ad93febd1a2563c0d63a52c952df09ecf29c
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-05 14:10:08 +0100

    passing feature test, implement turn based system

--- lib/game.rb
+++ lib/game.rb
@@ -5,9 +5,25 @@ class Game
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
+    @counter = 0
   end
 
   def attack(player)
+    return false unless correct_player?(player)
     player.receive_attack(Attack.new)
+    increment_counter
+  end
+
+  private
+  def correct_player?(attacked)
+    turn_to_move != attacked
+  end
+
+  def turn_to_move
+    @counter % 2 == 0 ? @player_one : @player_two
+  end
+
+  def increment_counter
+    @counter += 1
   end
 end
--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -10,10 +10,19 @@ RSpec.feature "Attack a player", :type => :feature do
   end
 
   scenario "playerTwo looses 10 health when attacked" do
-    p1_health_before = page.find_by_id("p1Health").text
+    p2_health_before = page.find_by_id("p2Health").text
+    attack = find('input[id="p1_attack"]')
+    attack.click
+    p2_health_after = page.find_by_id("p2Health").text
+    expect(p2_health_before.to_i - p2_health_after.to_i).to eq 10
+  end
+
+  scenario "playerOne attacks two times in a row" do
     attack = find('input[id="p2_attack"]')
     attack.click
+    p1_health = page.find_by_id("p1Health").text
+    attack.click
     p1_health_after = page.find_by_id("p1Health").text
-    expect(p1_health_before.to_i - p1_health_after.to_i).to eq 10
+    expect(p1_health.to_i - p1_health_after.to_i).to eq 0
   end
 end
